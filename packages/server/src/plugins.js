38fb95907601998f65ce0bc00a5dff4bcc8f777a
Valentin Vago <zeropaper@irata.ch>
2019-12-11 05:53:06 +0100

    move plugins to packages

--- packages/server/src/plugins.js
+++ packages/server/src/plugins.js
@@ -1,11 +1,11 @@
-const user = require('./plugins/user');
-// const twofa = require('./2fa');
-const client = require('./plugins/client');
-const organisations = require('./plugins/organisations');
-const groups = require('./plugins/groups');
-const activity = require('./plugins/activity');
-const docs = require('./plugins/docs');
-const webhook = require('./plugins/webhook');
+const user = require('../../users');
+// const twofa = require('../../fa');
+const client = require('../../client');
+const organisations = require('../../organisations');
+const groups = require('../../groups');
+const activity = require('../../activities');
+const docs = require('../../docs');
+const webhook = require('../../webhook');
 
 module.exports = [
   user,
