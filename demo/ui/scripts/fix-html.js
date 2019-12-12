8784d93776781d4e7928fffa25e244d2c3f27f71
Valentin Vago <zeropaper@irata.ch>
2019-12-12 17:47:21 +0100

    remove demo

--- demo/ui/scripts/fix-html.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-
-const filepath = path.resolve(__dirname, '../build/index.html');
-
-let content = fs.readFileSync(filepath, 'utf8');
-content = content.replace(/href="manifest/gm, () => 'href="/assets/icons/manifest');
-content = content.replace(/href="([^/])/gm, (match) => `href="/${match[match.length - 1]}`);
-fs.writeFileSync(filepath, content);
--- demo/ui/src/plugins/p-a/AView.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AView = () => (
-  <div>
-    View
-  </div>
-);
-
-export default AView;
