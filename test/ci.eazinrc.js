4a832bc9f60690841e08a88b816cd654f9332246
Valentin Vago <zeropaper@irata.ch>
2020-01-05 06:50:34 +0100

    attempt to fix GH actions

---
+++ test/ci.eazinrc.js
@@ -0,0 +1,26 @@
+const {
+  NODE_ENV = 'development',
+} = process.env;
+
+const config = {
+  env: NODE_ENV,
+  siteName: `eazin ${NODE_ENV}`,
+  baseURL: 'http://localhost:5000/',
+};
+
+// ------------------------------------------------------------
+
+config['eazin-mailer'] = {
+  siteSender: `${config.siteName} <no-reply@eazin.local>`,
+  // eslint-disable-next-line global-require
+  templates: require('../packages/users/src/server/user.email.templates'),
+  transport: 'test',
+};
+
+// ------------------------------------------------------------
+
+
+// eslint-disable-next-line no-console
+console.info('[eazin] using "%s" config', NODE_ENV);
+
+module.exports = config;
