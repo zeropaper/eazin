93e083a39f8dd5c5044c2cb0555514c39a2ca5a8
Valentin Vago <zeropaper@irata.ch>
2020-01-19 15:53:25 +0100

    change ui app load

--- src/ui/index.jsx
+++ src/ui/index.jsx
@@ -1,29 +1,46 @@
 /* eslint-env browser */
 /* eslint-disable react/jsx-filename-extension */
-import React, { lazy, Suspense } from 'react';
+
+// --------------------------------------------------
+
+import React from 'react';
 import { render } from 'react-dom';
 
-const App = lazy(() => import(/* webpackChunkName: "App" */ './App'));
+import App from './App';
 
 const root = document.getElementById('root');
+render(<App />, root);
 
-const Fallback = () => (
-  <div
-    style={{
-      position: 'absolute',
-      zIndex: 1000,
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontFamily: 'sans-serif',
-    }}
-  >
-    <span>Loading</span>
-  </div>
-);
-render(<Suspense fallback={<Fallback />}><App /></Suspense>, root);
+// --------------------------------------------------
+
+// import React, { lazy, Suspense } from 'react';
+// import { render } from 'react-dom';
+
+// const App = lazy(() => import(/* webpackChunkName: "App" */ './App'));
+
+// const root = document.getElementById('root');
+
+// const Fallback = () => (
+//   <div
+//     style={{
+//       position: 'absolute',
+//       zIndex: 1000,
+//       top: 0,
+//       left: 0,
+//       width: '100vw',
+//       height: '100vh',
+//       display: 'flex',
+//       flexDirection: 'column',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       fontFamily: 'sans-serif',
+//     }}
+//   >
+//     <span>Loading</span>
+//   </div>
+// );
+// render(<Suspense fallback={<Fallback />}><App /></Suspense>, root);
+
+// --------------------------------------------------
+
+if (module.hot) module.hot.accept();
