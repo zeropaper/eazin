90f92a79b62963ebe67487b840088b7db5e79747
Valentin Vago <zeropaper@irata.ch>
2020-01-26 08:53:21 +0100

    work on settings view

--- src/packages/settings/ui/SettingsView.jsx
+++ src/packages/settings/ui/SettingsView.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'eazin-core/ui';
 
-const SettingsView = () => (
+const yesNo = (bool) => (bool ? 'yes' : 'no');
+
+const SettingsView = ({ darkMode }) => (
   <div>
-    Settings View
+    Dark Mode:&nbsp;
+    {yesNo(darkMode)}
   </div>
 );
 
 SettingsView.pageTitle = 'Settings';
 
+SettingsView.propTypes = {
+  darkMode: PropTypes.bool,
+};
+
+SettingsView.defaultProps = {
+  darkMode: null,
+};
+
+const mapStateToProps = ({ settings }) => ({ ...settings });
+
+export default connect(mapStateToProps)(SettingsView);
