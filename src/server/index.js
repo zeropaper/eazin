76ed01cd9406afbbb8576e902cc45f4abe5b9acd
Valentin Vago <zeropaper@irata.ch>
2020-01-26 11:59:02 +0100

    cleanup project server

--- src/server/index.js
+++ src/server/index.js
@@ -1,27 +1,11 @@
 /* eslint-disable global-require */
-const path = require('path');
 const server = require('../packages/core/server');
 
 const {
   PORT = 5001,
-  PUBLIC_DIR = path.resolve(__dirname, '../../html-build'),
 } = process.env;
 
-server({
-  publicDir: PUBLIC_DIR,
-  plugins: [
-    require('../packages/users/server'),
-
-    require('../dev/kitchen-sink/server'),
-    require('../dev/groups/server'),
-    require('../dev/activities/server'),
-    require('../dev/clients/server'),
-    require('../dev/organisations/server'),
-    require('../dev/docs/server'),
-    // require('../dev/repositories/server'),
-    // require('../dev/ws/server'),
-  ],
-})
+server()
   .then((app) => {
     // eslint-disable-next-line no-console
     app.listen(PORT, () => console.info('API server listening on', PORT));
