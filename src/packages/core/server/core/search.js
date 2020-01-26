981e3ce215e42153c76963e02aa7cbbe9d73834a
Valentin Vago <zeropaper@irata.ch>
2020-01-26 08:31:45 +0100

    search return a promise

--- src/packages/core/server/core/search.js
+++ src/packages/core/server/core/search.js
@@ -27,45 +27,55 @@ const parseFilters = (filtersJSON) => {
 
 module.exports = function modelSearchPlugin(schema) { // , options) {
   schema.static('search', function modelSearch(query, next) {
-    const Model = this;
+    return new Promise((resolve, reject) => {
+      const Model = this;
+      const res = (result) => {
+        if (typeof next === 'function') next(null, result);
+        resolve(result);
+      };
+      const rej = (err) => {
+        if (typeof next === 'function') next(err);
+        reject(err);
+      };
 
-    const {
-      limit,
-      offset: skip,
-      // search,
-      orderBy,
-      orderDirection,
-    } = query;
+      const {
+        limit,
+        offset: skip,
+        // search,
+        orderBy,
+        orderDirection,
+      } = query;
 
-    const filter = parseFilters(query.filters);
-    Model.countDocuments(filter, (err, totalCount) => {
-      if (err) return next(err);
+      const filter = parseFilters(query.filters);
+      Model.countDocuments(filter, (err, totalCount) => {
+        if (err) return rej(err);
 
-      const absLimit = min(1000, abs(parseInt(limit, 10)) || 0);
+        const absLimit = min(1000, abs(parseInt(limit, 10)) || 0);
 
-      Model
-        .find(filter, null, {
-          skip: min(abs(parseInt(skip, 10)) || 0, totalCount),
-          limit: absLimit,
-        })
-        .sort(`${orderDirection === 'asc' ? '' : '-'}${orderBy || 'updatedAt'}`)
-        // eslint-disable-next-line no-shadow
-        .exec((err, data) => {
-          if (err) return next(err);
-
-          try {
-            next(null, {
-              data: Model.sanitizeOutput
-                ? data.map(Model.sanitizeOutput)
-                : data,
-              page: min(max(0, floor(skip / absLimit)), floor(totalCount / absLimit)) || 0,
-              totalCount,
-            });
+        Model
+          .find(filter, null, {
+            skip: min(abs(parseInt(skip, 10)) || 0, totalCount),
+            limit: absLimit,
+          })
+          .sort(`${orderDirection === 'asc' ? '' : '-'}${orderBy || 'updatedAt'}`)
           // eslint-disable-next-line no-shadow
-          } catch (err) {
-            next(err);
-          }
-        });
+          .exec((err, data) => {
+            if (err) return rej(err);
+
+            try {
+              res({
+                data: Model.sanitizeOutput
+                  ? data.map(Model.sanitizeOutput)
+                  : data,
+                page: min(max(0, floor(skip / absLimit)), floor(totalCount / absLimit)) || 0,
+                totalCount,
+              });
+            // eslint-disable-next-line no-shadow
+            } catch (err) {
+              rej(err);
+            }
+          });
+      });
     });
   });
 };
