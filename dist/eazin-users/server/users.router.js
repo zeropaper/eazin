3d1a537ce304cd782fd955403f45e8ec3b442ff9
Valentin Vago <zeropaper@irata.ch>
2020-02-06 06:56:19 +0100

    remove unneeded async on middleware

--- dist/eazin-users/server/users.router.js
+++ dist/eazin-users/server/users.router.js
@@ -42,7 +42,7 @@ router.patch(
   bearer,
   check(['patch:user']),
   requestHook('update user'),
-  async (req, res, next) => {
+  (req, res, next) => {
     if (!req.user.isAdmin) return next(httperrors.Forbidden());
     const User = req.db.model('User');
     const update = User.sanitizeInput(req.body);
