cfefa9f49b2343c52db3beb6c1d71319ecc30323
Valentin Vago <zeropaper@irata.ch>
2020-02-23 07:56:11 +0100

    change project app listen to use host + port

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,10 @@ const eazin = require('../packages/core/server');
 const { log } = eazin;
 
 eazin()
-  .then((app) => app.listen(app.get('port'), async () => {
+  .then((app) => app.listen({
+    port: app.get('port'),
+    host: app.get('host'),
+  }, () => {
     log(`API server listening on ${app.get('localURL')}`, app.get('externalAccessURL'));
   }))
   // eslint-disable-next-line no-console
