1ac465267d7e9e2805b4f832295bf932784cc181
Valentin Vago <zeropaper@irata.ch>
2020-03-01 07:36:06 +0100

    update example eazinrc

--- example.eazinrc.js
+++ example.eazinrc.js
@@ -1,50 +1,59 @@
-/* eslint-disable global-require */
-const path = require('path');
+const emailTemplates = require('eazin-users/server/user.email.templates');
 
 const {
-  PORT = 5001,
-  PUBLIC_DIR = path.resolve(__dirname, './html-build'),
+  PORT = 6060,
   NODE_ENV = 'development',
-  APP_ID = 'eazin',
+  MONGODB_URL = `mongodb://localhost:27017/eazin-site-${NODE_ENV}`,
+  NOTIFICATION_EMAIL,
+  NOTIFICATION_EMAIL_SERVER,
+  NOTIFICATION_EMAIL_PASS,
+  NOTIFICATION_EMAIL_PORT,
 } = process.env;
 
 const config = {
-  appId: APP_ID,
+  appId: 'eazin.site',
   port: PORT,
+  host: 'localhost',
   env: NODE_ENV,
-  publicDir: PUBLIC_DIR,
-  siteName: `eazin ${NODE_ENV}`,
-  baseURL: 'http://localhost:5000/',
-  dbURL: `mongodb://localhost:27017/${APP_ID}-${NODE_ENV}`,
+  publicDir: './build',
+  dbURL: MONGODB_URL,
+  siteName: 'eazin.dev',
+  localURL: NODE_ENV !== 'production'
+    ? `http://localhost:${PORT}/`
+    : 'https://example.com/',
+
   plugins: [
-    './src/packages/users/server',
-    // --------- vvvv DEV vvvv
-    './src/dev/kitchen-sink/server',
-    './src/dev/groups/server',
-    './src/dev/activities/server',
-    './src/dev/clients/server',
-    './src/dev/organisations/server',
-    './src/dev/docs/server',
+    'node_modules/eazin-users/server',
   ],
 };
 
 // ------------------------------------------------------------
 
 config['eazin-mailer'] = {
-  siteSender: `${config.siteName} <no-reply@eazin.local>`,
-  // eslint-disable-next-line global-require
-  templates: require('./src/packages/users/server/user.email.templates'),
-  // for NodemailerApp usage
-  transport: {
-    host: 'localhost',
-    port: 1025,
-    auth: {
-      user: 'project.1',
-      pass: 'secret.1',
+  siteSender: `${config.siteName} <no-reply@eazin.dev>`,
+  templates: emailTemplates,
+  transport: NODE_ENV !== 'production'
+    ? {
+      host: 'localhost',
+      port: 1025,
+      auth: {
+        user: 'project.2',
+        pass: 'secret.2',
+      },
+    } : {
+      host: NOTIFICATION_EMAIL_SERVER,
+      port: NOTIFICATION_EMAIL_PORT,
+      auth: {
+        user: NOTIFICATION_EMAIL,
+        pass: NOTIFICATION_EMAIL_PASS,
+      },
     },
-  },
 };
 
 // ------------------------------------------------------------
+
+
+// eslint-disable-next-line no-console
+console.info('[eazin] using "%s" config', NODE_ENV);
 
 module.exports = config;
