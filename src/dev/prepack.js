e1391b6a64ee98a3012bf8ebe12e4419f68eeef4
Valentin Vago <zeropaper@irata.ch>
2020-03-01 09:17:58 +0100

    attempt to fix versioning

---
+++ src/dev/prepack.js
@@ -0,0 +1,21 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const packageJSON = require('../../package.json');
+
+fs.readdirSync(path.resolve(__dirname, '../../dist')).forEach((pkgName) => {
+  const jsonFilepath = path.resolve(__dirname, '../../dist', pkgName, 'package.json');
+  const pkg = fs.readJSONSync(jsonFilepath);
+  console.info(pkg);
+  fs.writeFileSync(jsonFilepath, `${JSON.stringify({
+    ...pkg,
+    peerDependencies: Object.keys(pkg.peerDependencies).reduce((obj, key) => ({
+      ...obj,
+      [key]: key.startsWith(packageJSON.name)
+        ? pkg.version
+        : pkg.peerDependencies[key],
+    }), {}),
+  }, null, 2)}\n`);
+});
+
+console.info('@#@@######### POST PACK');
