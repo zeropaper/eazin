695f19190a97b39343b7c4280520843d689ec77d
Valentin Vago <zeropaper@irata.ch>
2020-03-14 07:46:14 +0100

    change breadcrumbs elevation

--- src/ui/BreadCrumbs.jsx
+++ src/ui/BreadCrumbs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar } from '@material-ui/core';
 
 export default () => (
-  <AppBar color="inherit" position="static" elevation={1}>
+  <AppBar color="inherit" position="static" elevation={2}>
     <Toolbar variant="dense">
       yo
     </Toolbar>
