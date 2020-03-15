576ede18effe545c485b74fea5305c75fa0fec2b
Valentin Vago <zeropaper@irata.ch>
2020-03-15 18:59:49 +0100

    fix storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,4 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js(|x)$/), module);
-configure(require.context('../src', true, /\.stories\.js(|x)$/), module);
+configure(require.context('../', true, /\.stories\.js(|x)$/), module);
